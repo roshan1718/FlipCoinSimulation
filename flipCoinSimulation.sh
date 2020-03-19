04435d911db5a1c3e80be04924ed511a7999f762
roshan1718 <roshanshinde1717@gmail.com>
2020-03-19 09:44:44 +0530

    ADDED code to store single combination in a dictionary

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,9 +4,38 @@
 # @Author : Roshan Balkrushna Shinde
 # @Since : 19-03-2020 
 
-if [[ $((RANDOM%2)) -eq 1 ]]
-then
-   echo Heads
-else
-   echo Tails
-fi
+singletDict=(["Heads"]=0 ["Tails"]=0)
+headCount=0
+tailCount=0
+noOfFlips=10
+
+function headOrTail()
+{
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		echo Heads
+		coinOutput="Heads"
+	else
+		echo Tails
+		coinOutput="Tails"
+	fi
+}
+
+function singletCombination()
+{
+	for (( index=0; index<$noOfFlips; index++ ))
+	do
+		headOrTail
+		if [[ $coinOutput == Heads ]]
+		then
+			singletDict[Heads]=$((${singletDict[Heads]}+1 ))
+			((headCount++))
+		else
+			singletDict[Tails]=$((${singletDict[Tails]}+1 ))
+			((tailCount++))
+		fi
+	done
+	headPercentage=`expr "scale=2; $headCount * 100 / $noOfFlips" | bc`
+	tailPercentage=`expr "scale=2; $tailCount * 100 / $noOfFlips" | bc`
+}
+singletCombination
